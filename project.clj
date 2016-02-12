1998143949537e75dd9c2f473e32f403d8479e5f
Stuart Sierra <mail@stuartsierra.com>
2016-02-12 10:50:05 -0500

    Update dependencies and add exclusions

--- project.clj
+++ project.clj
@@ -3,23 +3,41 @@
   :url "https://github.com/stuartsierra/log.dev"
   :dependencies
   [;; Use Logback as the main logging implementation:
-   [ch.qos.logback/logback-classic "1.1.2"]
+   [ch.qos.logback/logback-classic "1.1.3"]
+   [ch.qos.logback/logback-core "1.1.3"]
 
    ;; Logback implements the SLF4J API:
-   [org.slf4j/slf4j-api "1.7.7"]
+   [org.slf4j/slf4j-api "1.7.16"]
 
    ;; Redirect Apache Commons Logging to Logback via the SLF4J API:
-   [org.slf4j/jcl-over-slf4j "1.7.7"]
+   [org.slf4j/jcl-over-slf4j "1.7.16"]
 
    ;; Redirect Log4j 1.x to Logback via the SLF4J API:
-   [org.slf4j/log4j-over-slf4j "1.7.7"]
+   [org.slf4j/log4j-over-slf4j "1.7.16"]
 
-   ;; Redirect Log4J 2.x to Logback via the SLF4J API:
-   [org.apache.logging.log4j/log4j-to-slf4j "2.0.2"]
+   ;; Redirect Log4j 2.x to Logback via the SLF4J API:
+   [org.apache.logging.log4j/log4j-to-slf4j "2.5"]
+
+   ;; Redirect OSGI LogService to Logback via the SLF4J API
+   [org.slf4j/osgi-over-slf4j "1.7.16"]
 
    ;; Redirect java.util.logging to Logback via the SLF4J API.
    ;; Requires installing the bridge handler, see README:
-   [org.slf4j/jul-to-slf4j "1.7.7"]]
+   [org.slf4j/jul-to-slf4j "1.7.16"]]
+
+  :exclusions
+  [;; Exclude transitive dependencies on all other logging
+   ;; implementations, including other SLF4J bridges.
+   commons-logging
+   log4j
+   org.apache.logging.log4j/log4j
+   org.slf4j/simple
+   org.slf4j/slf4j-api
+   org.slf4j/slf4j-jcl
+   org.slf4j/slf4j-nop
+   org.slf4j/slg4j-log4j12
+   org.slf4j/slg4j-log4j13
+   ]
   
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [org.clojure/tools.logging "0.3.1"]
