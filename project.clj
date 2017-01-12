01a7ed2addbe8a5fedcefcfddc12082b06114858
Stuart Sierra <mail@stuartsierra.com>
2017-01-12 13:33:10 -0500

    Set next development version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stuartsierra/log.dev "0.1.1-SNAPSHOT"
+(defproject com.stuartsierra/log.dev "0.2.0-SNAPSHOT"
   :description "Quick-start logging for local development using Logback and SLF4J"
   :url "https://github.com/stuartsierra/log.dev"
   :dependencies
